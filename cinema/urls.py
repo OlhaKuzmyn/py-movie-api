f7e5692107e766f5ce24252374c7ef6d3b57a0fb
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-09-12 17:08:46 +0300

    added trailing slash

--- cinema/urls.py
+++ cinema/urls.py
@@ -5,6 +5,6 @@ from cinema.views import movie_list, movie_detail
 app_name = "cinema"
 
 urlpatterns = [
-    path("cinema/movies", movie_list, name="movie_list"),
-    path("cinema/movies/<int:pk>", movie_detail, name="movie_detail"),
+    path("cinema/movies/", movie_list, name="movie_list"),
+    path("cinema/movies/<int:pk>/", movie_detail, name="movie_detail"),
 ]
