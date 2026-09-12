a9dad3df9b170253d5093b41864916205121d11c
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-09-12 16:55:29 +0300

    added movie detail view

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -6,7 +6,7 @@ from cinema.models import Movie
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField()
-    description = serializers.CharField()
+    description = serializers.CharField(required=False)
     duration = serializers.IntegerField()
 
     def create(self, validated_data):
--- cinema/urls.py
+++ cinema/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from cinema.views import movie_list
+from cinema.views import movie_list, movie_detail
 
 app_name = "cinema"
 
 urlpatterns = [
     path("cinema/movies", movie_list, name="movie_list"),
+    path("cinema/movies/<int:pk>", movie_detail, name="movie_detail"),
 ]
